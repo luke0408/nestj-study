0b3f2a06f5b351a28052708b44db2daabd1988cf
zjvl324 <zjvl324@gmail.com>
2024-12-18 13:42:33 +0000

    ✨ [Feat] 전체 게시물 조회 api

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Get, Controller } from "@nestjs/common";
 import { BoardService } from "./boards.service";
+import { Board } from "./board.model";
 
 @Controller('boards')
 export class BoardController {
     constructor(private boardService: BoardService) {}
+
+    @Get('/')
+    getAllBoard(): Board[] {
+        return this.boardService.getAllBoards();
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,4 +4,8 @@ import { Board } from "./board.model";
 @Injectable()
 export class BoardService {
     private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
 }
