240033644db50ea414a027bbef0a9e75b3d76ed7
zjvl324 <zjvl324@gmail.com>
2024-12-18 14:00:25 +0000

    ✨ [Feat] 삭제 기능 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Param, Body, Get, Post, Controller } from "@nestjs/common";
+import { Param, Body, Get, Post, Delete, Controller } from "@nestjs/common";
 import { BoardService } from "./boards.service";
 import { Board } from "./board.model";
 import { CreateBoardDto } from "./dto/create-board.dto";
@@ -20,5 +20,10 @@ export class BoardController {
     @Get('/:id')
     getBoardById(@Param('id') id: string): Board {
         return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoardById(@Param('id') id: string): void {
+        return this.boardService.deleteBoardById(id);
     }
 }
