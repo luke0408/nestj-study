1d73cdc398c790d31fb7bfeca929c15647180c99
zjvl324 <zjvl324@gmail.com>
2025-01-01 13:51:24 +0000

    ✨ [Feat] repository 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Param, Body, Get, Post, Delete, Patch, Controller, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Board } from './board.entity';
+import { BoardStatus } from './board.status.enum';
 import { BoardService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -9,28 +10,28 @@ export class BoardController {
   constructor(private boardService: BoardService) {}
 
   @Get('/')
-  getAllBoard(): Board[] {
+  getAllBoard(): Promise<Board[]> {
     return this.boardService.getAllBoards();
   }
 
   @Post('/')
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardService.createBoard(createBoardDto);
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardById(@Param('id') id: number): Promise<Board> {
     return this.boardService.getBoardById(id);
   }
 
   @Delete('/:id')
-  deleteBoardById(@Param('id') id: string): void {
+  deleteBoardById(@Param('id') id: number): Promise<void> {
     return this.boardService.deleteBoardById(id);
   }
 
   @Patch('/:id/status')
-  updateBoardStatus(@Param('id') id: string, @Body('status', BoardStatusValidationPipe) status: BoardStatus) {
+  updateBoardStatus(@Param('id') id: number, @Body('status', BoardStatusValidationPipe) status: BoardStatus) {
     return this.boardService.updateBoardStatus(id, status);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,46 +1,52 @@
-import { v4 as uuid } from 'uuid';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
+import { BoardRespository } from './board.repository';
+import { BoardStatus } from './board.status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardService {
-  private boards: Board[] = [];
+  constructor(
+    @InjectRepository(BoardRespository)
+    private boardRepository: BoardRespository,
+  ) {}
 
-  createBoard(createBoardDto: CreateBoardDto) {
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
+
+    const board = this.boardRepository.create({
+      title: title,
+      descrition: description,
       status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
+    });
+
+    await this.boardRepository.save(board);
     return board;
   }
 
-  updateBoardStatus(id: string, status: BoardStatus) {
-    const board = this.getBoardById(id);
+  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+    const board = await this.getBoardById(id);
     board.status = status;
+    await this.boardRepository.save(board);
     return board;
   }
 
-  getAllBoards(): Board[] {
-    return this.boards;
+  async getAllBoards(): Promise<Board[]> {
+    return await this.boardRepository.find();
   }
 
-  getBoardById(id: string): Board {
-    const found = this.boards.find((board) => board.id === id);
+  async getBoardById(id: number): Promise<Board> {
+    const board = await this.boardRepository.findOneBy({ id });
 
-    if (!found) {
+    if (!board) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
     }
 
-    return found;
+    return board;
   }
 
-  deleteBoardById(id: string): void {
-    const found = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== found.id);
+  async deleteBoardById(id: number): Promise<void> {
+    await this.boardRepository.softDelete(id);
   }
 }
