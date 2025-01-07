7dd2ced44d0c51f01962c9dc9364c9b7c81d993f
zjvl324 <zjvl324@gmail.com>
2025-01-07 13:25:27 +0000

    ✨ [Feat] Response From
    <footer>
    - 관련: #3 #7

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,10 @@
 export type Merge<F, S> = {
     [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
 };
+
+export interface ResponseForm<T> {
+    result: true;
+    code: 1000;
+    requestToResponse?: `${number}ms`;
+    data: T;
+}
