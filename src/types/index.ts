b3a4134283f66921b31108f4d1d61c302ed64b82
zjvl324 <zjvl324@gmail.com>
2025-01-07 14:00:16 +0000

    ✨ [Feat] Type Discription & Error Util Types
    <footer>
    - 관련: #3 #7

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,26 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+import type { ERROR } from "src/configs/errors";
+
 /**
- * Merge two types into a new type.
- * This type can be useful to override or add fields to a type.
+ * Common Util Types
  */
 export type Merge<F, S> = {
     [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
 };
 
+/**
+ * Response Form
+ */
 export interface ResponseForm<T> {
     result: true;
     code: 1000;
     requestToResponse?: `${number}ms`;
     data: T;
 }
+
+/**
+ * Error Util Types
+ */
+export type KeyOfError = keyof ERROR;
+export type ValueOfError = ERROR[KeyOfError];
