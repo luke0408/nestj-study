52d1415d4bcf8425c48362c68985340f2081270a
zjvl324 <zjvl324@gmail.com>
2025-01-07 14:15:34 +0000

    ✨ [Feat] pagination type
    <footer>
    - 관련: #3 #7

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,21 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-
-import type { ERROR } from "src/configs/errors";
+import type { ERROR } from 'src/configs/errors';
 
 /**
  * Common Util Types
  */
 export type Merge<F, S> = {
-    [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
+  [K in keyof (F & S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never;
 };
 
 /**
  * Response Form
  */
 export interface ResponseForm<T> {
-    result: true;
-    code: 1000;
-    requestToResponse?: `${number}ms`;
-    data: T;
+  result: true;
+  code: 1000;
+  requestToResponse?: `${number}ms`;
+  data: T;
 }
 
 /**
@@ -24,3 +23,11 @@ export interface ResponseForm<T> {
  */
 export type KeyOfError = keyof ERROR;
 export type ValueOfError = ERROR[KeyOfError];
+
+/**
+ * Pagination Types
+ */
+export interface InitialPaginationResponseType {
+  list: any[];
+  count: number;
+}
