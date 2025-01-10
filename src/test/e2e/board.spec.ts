f3cb4da7844a568afcfdfe5e1a2212563ba84318
zjvl324 <zjvl324@gmail.com>
2025-01-10 14:36:41 +0000

    🧪 [Test] 기존 e2e test 코드 삭제 및 board e2e test 세팅
    <footer>
    - 관련: #3 #8

---
+++ src/test/e2e/board.spec.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { after, before } from "node:test";
+import { AppModule } from "../../app.module";
+
+describe('E2E board test', () => {
+    const host = 'http://localhost:4000';
+    let app: INestApplication;
+    let testingModule: TestingModule;
+
+    before(async () => {
+        testingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = testingModule.createNestApplication();
+        await (await app.init()).listen(4000);
+    });
+
+    after(async () => {
+        await app.close();
+    });
+})
