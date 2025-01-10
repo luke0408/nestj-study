ae8891eda9ed53f4514f974337549a0b310ddaa6
zjvl324 <zjvl324@gmail.com>
2025-01-10 14:35:05 +0000

    ♻️ [Refactor] entity src 수정
    <footer>
    - 관련: #3 #8

--- data-source.ts
+++ data-source.ts
@@ -1,22 +1,19 @@
-import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import { DataSource } from 'typeorm';
 
 dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV;
 
 export default new DataSource({
-    type: 'mysql',
-    host: process.env[`${NODE_ENV}_DB_HOST`] as string ,
-    port: Number(process.env[`${NODE_ENV}_DB_PORT`]) as number,
-    username: process.env[`${NODE_ENV}_DB_USERNAME`] as string,
-    password: process.env[`${NODE_ENV}_DB_PASSWORD`] as string,
-    database: process.env[`${NODE_ENV}_DB_DATABASE`] as string,
-    entities: [
-        path.join(__dirname, './src/entities/*.entity.ts'),
-        path.join(__dirname, './src/entities/*.entity.js'),
-    ],
-    synchronize: false,
-    logging: true,
-})
+  type: 'mysql',
+  host: process.env[`${NODE_ENV}_DB_HOST`] as string,
+  port: Number(process.env[`${NODE_ENV}_DB_PORT`]) as number,
+  username: process.env[`${NODE_ENV}_DB_USERNAME`] as string,
+  password: process.env[`${NODE_ENV}_DB_PASSWORD`] as string,
+  database: process.env[`${NODE_ENV}_DB_DATABASE`] as string,
+  entities: [path.join(__dirname, './src/models/tables/*.{ts, js}')],
+  synchronize: false,
+  logging: true,
+});
