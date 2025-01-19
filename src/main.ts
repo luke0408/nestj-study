de7aa1bcb39814ecf9698247e540ad76aefe9594
zjvl324 <zjvl324@gmail.com>
2025-01-19 07:18:47 +0000

    🔧 [Chore] runtime swagger 설정
    <footer>
    - 관련: #3 #5

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestiaSwaggerComposer } from '@nestia/sdk';
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const document = await NestiaSwaggerComposer.document(app, {
+    openapi: "3.1",
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local server"
+      }
+    ]
+  });
+  SwaggerModule.setup("api", app, document as any);
   await app.listen(3000);
 }
 
